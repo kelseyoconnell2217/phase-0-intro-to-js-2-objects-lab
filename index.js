e0330a8f1b38d3527622e2dab93593bbe0efe2b1
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2023-12-02 14:53:51 -0500

    Completed Labgit add .!

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+const employee = {
+    name: "Kelsey",
+    streetAddress: "2217 Linden St."
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employee2 = {...employee}; 
+    employee2[key] = value
+    return employee2
+}
+updateEmployeeWithKeyAndValue(employee, name1, "Sam")
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, value2) {
+    employee[streetAddress] = value2
+    return employee
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway')
+
+function deleteFromEmployeeByKey(employee, key){
+    let newEmployee = {...employee}; 
+    delete newEmployee[key]
+    return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    let newEmployee = employee 
+    delete employee['name']
+    return employee
+}
+destructivelyDeleteFromEmployeeByKey(employee, 'name')
+
