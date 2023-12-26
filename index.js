cf402fb84f94b9425793fdc612959ab5601895db
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2023-12-26 02:35:25 -0500

    Lab Completed Again

--- index.js
+++ index.js
@@ -3,29 +3,23 @@ const employee = {
     streetAddress: "2217 Linden St."
 }
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const employee2 = {...employee}; 
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employee2 = {...employee} 
     employee2[key] = value
     return employee2
 }
-updateEmployeeWithKeyAndValue(employee, name1, "Sam")
 
-
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, value2) {
-    employee[streetAddress] = value2
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
     return employee
 }
-destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway')
-
+const newEmployee = {...employee}
 function deleteFromEmployeeByKey(employee, key){
-    let newEmployee = {...employee}; 
     delete newEmployee[key]
     return newEmployee
 }
-function destructivelyDeleteFromEmployeeByKey(employee, name) {
-    let newEmployee = employee 
-    delete employee['name']
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
     return employee
 }
-destructivelyDeleteFromEmployeeByKey(employee, 'name')
-
